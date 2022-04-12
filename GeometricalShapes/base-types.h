ddd1547be35b54669369ae5403182dec100049ee
AnatoliyProhortsev <tolik.spb.ru@yandex.ru>
2022-04-12 14:28:53 +0300

    Убрал конструкторы у _t структур

--- GeometricalShapes/base-types.h
+++ GeometricalShapes/base-types.h
@@ -4,14 +4,11 @@ namespace prohorcev
 {
 	struct point_t
 	{
-		point_t(double xcoord, double ycoord) : x(xcoord), y(ycoord) {}
 		double x, y;
 	};
 
 	struct rectangle_t
 	{
-		rectangle_t(point_t p, double w, double h) : pos(p), width(w), height(h) {}
-		rectangle_t(double x, double y, double w, double h) : pos(x, y), width(w), height(h) {}
 		double width, height;
 		point_t pos;
 	};
--- GeometricalShapes/rectangle.cpp
+++ GeometricalShapes/rectangle.cpp
@@ -2,17 +2,22 @@
 
 using namespace prohorcev;
 
-Rectangle::Rectangle(double x_lower_left, double y_lower_left, double x_upper_right, double y_upper_right): m_rect(0, 0, 0, 0)
+Rectangle::Rectangle(double x_lower_left, double y_lower_left, double x_upper_right, double y_upper_right)
 {
 	if (x_lower_left >= x_upper_right || y_lower_left >= y_upper_right)
 	{
 		std::cout << "Неверные параметры прямоугольника. Создание прямоугольника с точками (0, 0, 0, 0)\n";
+		m_rect.height = 0;
+		m_rect.width = 0;
+		m_rect.pos.x = 0;
+		m_rect.pos.y = 0;
 	}
 	else
 	{
-		m_rect = rectangle_t((x_upper_right + x_lower_left) / 2,
-							(y_upper_right + y_lower_left) / 2,
-							 x_upper_right - x_lower_left, y_upper_right - y_lower_left);
+		m_rect.height = y_upper_right - y_lower_left;
+		m_rect.width = x_upper_right - x_lower_left;
+		m_rect.pos.x = (x_upper_right + x_lower_left) / 2;
+		m_rect.pos.y = (y_upper_right + y_lower_left) / 2;
 	}
 }
 
@@ -50,6 +55,7 @@ void Rectangle::scale(double coefficient)
 
 void Rectangle::print_coordinates()
 {
-	std::cout <<std::setprecision(2)<< "Прямоугольник с координатами:\n" << double(m_rect.pos.x - m_rect.width / 2) << " " << double(m_rect.pos.y - m_rect.height / 2) << " "
+	std::cout <<std::setprecision(2)<< "Прямоугольник с координатами:\n" 
+												   << double(m_rect.pos.x - m_rect.width / 2) << " " << double(m_rect.pos.y - m_rect.height / 2) << " "
 												   << double(m_rect.pos.x + m_rect.width / 2) << " " << double(m_rect.pos.y + m_rect.height / 2) << '\n';
 }
